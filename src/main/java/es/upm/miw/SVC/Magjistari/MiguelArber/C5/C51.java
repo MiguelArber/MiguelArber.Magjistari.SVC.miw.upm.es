300f41da595fccf53dcb4895fde8361f99eec9cd
MiguelArber <miguel.arber@gmail.com>
2017-09-25 15:28:11 +0200

    Added class C51

---
+++ src/main/java/es/upm/miw/SVC/Magjistari/MiguelArber/C5/C51.java
@@ -0,0 +1,17 @@
+package es.upm.miw.SVC.Magjistari.MiguelArber.C5;
+
+public class C51 {
+	
+    public String m1() {
+    	
+        return "m1";
+       
+    }
+    
+    public String m2() {
+    	
+        return "m2";
+       
+    }
+
+}
